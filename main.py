ed6f47aa7bf2783a8cc523e5afdd60963d204a09
Telegrammer <61018335+telegrammer@users.noreply.github.com>
2021-02-10 17:46:43 +0300

    Решени для второй задачи

--- main.py
+++ main.py
@@ -4,41 +4,52 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/"
-
-lon = "37.530887"
-lat = "55.703118"
-delta = "0.002"
-
-params = {
-    "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-response = requests.get(map_request, params=params)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
+delta = 0.001
+zoom_count = 0
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+map_request = "http://static-maps.yandex.ru/1.x/"
+params = {'ll': '37.530887,55.703118',
+          'l': 'map'
+          }
+
+
+def picture_setup():
+    params['spn'] = f'{delta},{delta}'
+    try:
+        response = requests.get(map_request, params=params)
+        assert response
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        try:
+            screen.blit(pygame.image.load(map_file), (0, 0))
+        except pygame.error:
+            pass
+    except AssertionError:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.K_PAGEUP:
-            print('НУ ТЫ И ЛОХ')
-
+picture_setup()
+while pygame.event.wait().type != pygame.QUIT:
+    if pygame.key.get_pressed():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_PAGEDOWN]:
+            delta /= 10
+            if delta >= 0.001:
+                picture_setup()
+            else:
+                delta = 0.001
+        elif keys[pygame.K_PAGEUP]:
+            delta *= 10
+            if delta < 100.0:
+                picture_setup()
+            else:
+                delta = 100.0
     pygame.display.flip()
-pygame.quit()
 
+pygame.quit()
 os.remove(map_file)
